bce12c2ca3d62d2868779f02318f8a4f9585d367
VictorPilipenko <thejustvic@gmail.com>
2018-07-19 18:48:49 +0300

    configure DashBoard

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import ClientList from './ClientList';
+import ClientFilter from './ClientFilter';
+import ClientDetail from './ClientDetail';
 
 const DashBoard = () => (
     <div> 
-           
+           <div className='container__list'>
+                <ClientFilter />
+                <ClientList />
+            </div>
+
+
+             <div className='container__box'>      
+                 <ClientDetail />
+            </div>
     </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import allReducers from './reducers/allReducers';
+import { Provider } from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
 import './styles/styles.scss';
 import registerServiceWorker from './registerServiceWorker';
+import 'semantic-ui-css/semantic.min.css';
 
-
-
+const logger = createLogger();
+const store = createStore(
+    allReducers,
+    applyMiddleware(thunk, promise, logger)
+);
 
 ReactDOM.render(
-    <AppRouter />,
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>,
     document.getElementById('app')
 );
 registerServiceWorker();
